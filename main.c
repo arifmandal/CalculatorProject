2dc0412fcc287c835db86a98a281abb1337e168b
Arif Mandal <arifmandal96@gmail.com>
2022-12-09 21:21:21 +0300

    Added Module function

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ int main()
     printf("subtraction: %d\n", sub(number1,number2) );
     printf("multiplication: %d\n", mult(number1,number2) );
     printf("division: %d\n", division(number1,number2) );
+    printf("module: %d\n", modul(number1,number2) );
     printf("Program is ended...Good Bye...\n");
     return 0;
 }
@@ -31,4 +32,8 @@ int mult(int number1, int number2){
 
 int division(int number1, int number2){
     return number1/number2;
+}
+
+int modul(int number1, int number2){
+    return number1%number2;
 }
