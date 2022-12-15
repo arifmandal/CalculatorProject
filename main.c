2ab05f8c00e644be117b42613243fef8a4ad34f5
Arif Mandal <arifmandal96@gmail.com>
2022-12-15 10:49:00 +0300

    Added printInfo function.

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int sub(int number1, int number2);
 int mult(int number1, int number2);
 int division(int number1, int number2);
 int modul(int number1, int number2);
-
+void printInfo(int number1, int number2);
 int main()
 {
     printf("Calculator Project!\n");
@@ -14,13 +14,17 @@ int main()
     scanf("%d", &number1);
     printf("Enter the second number:\n");
     scanf("%d", &number2);
+    printInfo(number1, number2);
+    printf("Program is ended...Good Bye...\n");
+    return 0;
+}
+
+void printInfo(int number1, int number2){
     printf("addition: %d\n", add(number1,number2) );
     printf("subtraction: %d\n", sub(number1,number2) );
     printf("multiplication: %d\n", mult(number1,number2) );
     printf("division: %d\n", division(number1,number2) );
     printf("module: %d\n", modul(number1,number2) );
-    printf("Program is ended...Good Bye...\n");
-    return 0;
 }
 
 int add(int number1, int number2){
